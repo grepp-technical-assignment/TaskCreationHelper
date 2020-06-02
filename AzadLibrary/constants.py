7eb425a2917b94b912d3e990a11dd4efccc473eb
Azad <spongbob9876@naver.com>
2020-06-02 20:02:52 +0900

    Allow int on float parameter types

--- AzadLibrary/constants.py
+++ AzadLibrary/constants.py
@@ -70,12 +70,12 @@ IODataTypesInfo = {
         "strize": (lambda x: "%d" % (x,)),
     },
     "float": {
-        "pytypes": (float, Decimal, Fraction),
+        "pytypes": (float, Decimal, Fraction, int),
         "constraint": (lambda x: 1.175494351e-38 <= abs(x) <= 3.402823466e38 or x == 0),
         "strize": __IODataTypesInfo_FloatStrize,
     },
     "double": {
-        "pytypes": (float, Decimal, Fraction),
+        "pytypes": (float, Decimal, Fraction, int),
         "constraint": (lambda x: float_info.min <= abs(x) <= float_info.max or x == 0),
         "strize": __IODataTypesInfo_FloatStrize,
     },

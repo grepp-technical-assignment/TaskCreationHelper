1664a3b85567db3af0e81aa0e2aad7720b88b4b9
Azad <spongbob9876@naver.com>
2020-09-22 15:09:26 +0900

    Changes on run.py

--- run.py
+++ run.py
@@ -14,33 +14,21 @@ if __name__ == "__main__":
 
     # Azad library
     from AzadLibrary import AzadCore
-    from AzadLibrary.constants import StartingConfigState, DefaultStatement
+    import AzadLibrary.constants as Const
     from AzadLibrary.misc import barLine
 
     subcommand = argv[1] if len(argv) > 1 else "help"
     print(barLine("Azad Library"))
     atexit.register(print, barLine("Azad Library Termination"))
-    startInformation = deepcopy(StartingConfigState)
     logging.captureWarnings(True)
 
     if subcommand == "help":  # Help
-        helpStr = """
-<Documentation>
-Usage: python3 run.py (subcommand) [args...]
-List of subcommands:
-    - help: "python3 run.py help"
-        Print documentation.
-    - full: "python3 run.py full [running_path / config_file_path]"
-        Run full process - I/O data generation, source file validation, etc.
-        You need a config.json file in running path.
-    - produce_io: "python3 run.py produce_io [running_path / config_file_path]"
-        Minimize validation process and produce I/O data file only.
-        You need a config.json file in running path.
-"""
-        print(helpStr)
+        with open(Const.ResourcesPath / "etc/help.txt", "r") as helpFile:
+            print(helpFile.read())
 
     elif subcommand == "init":  # Interactive initialization
 
+        startInformation = deepcopy(Const.StartingConfigState)
         print("Let's initialize new problem folder.")
         print("Current location is \"%s\"." % (os.getcwd(),))
         folderNamePattern = re.compile("[A-Za-z0-9_]+(/[A-Za-z0-9_]+)*")
@@ -65,9 +53,11 @@ List of subcommands:
         with open(Path(folderName) / "config.json", "w") as configFile:
             json.dump(startInformation, configFile, indent=4)
         with open(Path(folderName) / "statement.md", "w") as statementFile:
-            statementFile.write(DefaultStatement)
+            with open(Const.ResourcesPath / "etc/default_statement.md", "r") \
+                    as defaultStatementFile:
+                statementFile.write(defaultStatementFile.read())
 
-    elif subcommand in ("full", "produce_io"):  # Actual run process
+    elif subcommand in ("full", "produce", "generate"):  # Actual run process
 
         # Check config.json file
         configFilePath = Path(argv[2] if len(argv) > 2 else os.getcwd())
@@ -83,16 +73,14 @@ List of subcommands:
             if not configFilePath.exists():
                 raise FileNotFoundError("%s not found" % (configFilePath,))
 
+        # Run total pipeline
         Core = AzadCore(configFilePath)
-        if subcommand == "full":
-            Core.checkAllSolutionFiles()
-            Core.makeInputFiles()
-            Core.makeOutputFiles()
-        elif subcommand == "produce":
-            Core.makeInputFiles()
-            Core.makeOutputFiles()
-        else:
-            raise NotImplementedError
+        mode: Const.AzadLibraryMode = {
+            "full": Const.AzadLibraryMode.Full,
+            "produce": Const.AzadLibraryMode.Produce,
+            "generate": Const.AzadLibraryMode.GenerateCode,
+        }[subcommand]
+        Core.run(mode)
 
     else:
         raise ValueError(

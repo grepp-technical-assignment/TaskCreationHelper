63adea59e17e2547fc19a8e574d31ed105c0f588
Azad <spongbob9876@naver.com>
2020-09-24 03:26:53 +0900

    Added TCH helper (header-only) libraries

---
+++ AzadLibrary/resources/helpers/tchio.hpp
@@ -0,0 +1,104 @@
+#ifndef __TCH_IO_HEADER
+#define __TCH_IO_HEADER
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdexcept>
+
+namespace TCH{
+
+    // Validate if given 2d array is perfect rectangle.
+    template <typename t>
+    bool validateRectangle(const std::vector<std::vector<t>> &val){
+        for(size_t r=1; r<val.size(); r++) 
+            if(val[r].size() != val[0].size()) return false;
+        return true;
+    }
+
+    // Get single variable from input.
+    template <typename t> 
+    t get0d(std::istream &in){
+        t val; in >> val;
+        return val;
+    }
+    // Bool specialization of get0d.
+    template <> 
+    bool get0d<bool>(std::istream &in){
+        std::string msg; in >> msg;
+        if(msg != "true" && msg != "false")
+            throw std::runtime_error("Boolean parsing failed");
+        return msg == "true";
+    }
+    // String specialization of get0d.
+    template <>
+    std::string get0d<std::string>(std::istream &in){
+        std::string result;
+        int len = get0d<int>(in);
+        while(len--){
+            int lastchar = (char)get0d<int>(in);
+            if(lastchar < 0 || lastchar > 255)
+                throw std::runtime_error("Non-ascii character received");
+            result += (char)lastchar;
+        }
+        return result;
+    }
+
+    // Get 1d array from input.
+    template <typename t> 
+    std::vector<t> get1d(std::istream &in){
+        int size; in >> size;
+        if(size < 0) throw std::runtime_error("Non-negative size");
+        std::vector<t> result;
+        while(size--) result.push_back(get0d<t>(in));
+    }
+
+    // Get 2d array from input.
+    template <typename t> 
+    std::vector<std::vector<t>> get2d(
+            std::istream &in, bool validateRec = true){
+        int size; in >> size;
+        if(size < 0) throw std::runtime_error("Non-negative size");
+        std::vector<std::vector<t>> result;
+        while(size--) result.push_back(get1d<t>(in));
+        if(validateRec) if(!validateRectangle<t>(result))
+            throw std::runtime_error("Given array is not rectangle");
+        return result;
+    }
+
+    // Put single variable to output.
+    template <typename t> 
+    void put0d(std::ostream &out, const t &val){
+        out << val << '\n';
+    }
+    // Bool specialization of put0d.
+    template <>
+    void put0d<bool>(std::ostream &out, const bool &val){
+        out << (val ? "true" : "false") << '\n';
+    }
+    // String specialization of put0d.
+    template<>
+    void put0d<std::string>(std::ostream &out, const std::string &val){
+        put0d<size_t>(out, val.size());
+        for(char c: val) put0d<int>(out, (int)c);
+    }
+
+    // Put 1d array to output.
+    template <typename t> 
+    void put1d(std::ostream &out, const std::vector<t> &val){
+        put0d<size_t>(out, val.size());
+        for(t element: val) put0d<t>(out, element);
+    }
+
+    // Put 2d array to output.
+    template <typename t>
+    void put2d(std::ostream &out, const std::vector<std::vector<t>> &val,
+               bool validateRec = true){
+        if(validateRec) if(!validateRectangle<t>(val))
+            throw std::runtime_error("Given array is not rectangle");
+        put0d<size_t>(out, val.size());
+        for(t element: val) put1d<t>(out, element);
+    }
+};
+
+#endif
---
+++ AzadLibrary/resources/helpers/tchrand.hpp
@@ -0,0 +1,87 @@
+#ifndef __TCH_RAND_HEADER
+#define __TCH_RAND_HEADER
+
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <random>
+#include <stdexcept>
+
+namespace TCH{
+
+    // Random generator engine
+    std::mt19937_64 mersenneTwister;
+
+    // Set seed of engine. This should be called before generator function.
+    void seed(const std::vector<std::string> &genscript){
+        if(genscript.empty()) throw std::runtime_error("Empty genscript given");
+
+        // Join all strings in genscript
+        std::string joined;
+        for(std::string arg: genscript){
+            joined += arg; 
+            joined += '|';
+        } joined.pop_back();
+
+        // Make seed sequence and apply
+        std::seed_seq seed(joined.begin(), joined.end());
+        mersenneTwister.seed(seed);
+    }
+
+    // Return uniform integer on range [l, r].
+    template <typename intlike> intlike randint(intlike l, intlike r){
+        std::uniform_int_distribution<intlike> distrib(l, r);
+        return distrib(mersenneTwister);
+    }
+
+    // Return uniform real number on range [l, r).
+    template <typename reallike> reallike randreal(reallike l, reallike r){
+        std::uniform_real_distribution<reallike> distrib(l, r);
+        return distrib(mersenneTwister);
+    }
+
+    // Return True or False randomly.
+    bool randbool(){
+        std::uniform_int_distribution<int> distrib(0, 1);
+        return (bool)distrib(mersenneTwister);
+    }
+
+    // Generate random permutation with given size.
+    std::vector<int> generatePermutation(int size){
+        if(size <= 0) throw "Non-positive size given";
+        std::vector<int> result(size);
+        for(int i=0; i<size; i++) result[i] = i;
+        for(int i1=size-1; i1>=0; i1--){
+            int i2 = randint<int>(0, i1);
+            if(i1 != i2) std::swap(result[i1], result[i2]);
+        } return result;
+    }
+
+    // Shuffle between two iterators.
+    template <class iterator> void shuffle(iterator begin, iterator end){
+        int size = std::distance<iterator>(begin, end);
+        if(size < 0) throw "End iterator is at before begin iterator";
+        else if(size == 0) return;
+        
+        // arr[x] := arr[perm[x]]
+        std::vector<int> perm = generatePermutation(size);
+        std::vector<bool> moved(size, false);
+
+        // Iterate
+        for(iterator it = begin; it != end; it++){
+            if(moved[it - begin]) continue;
+
+            // itprev denotes arr[x], itafter denotes arr[perm[x]]
+            iterator itprev = it, itafter = begin + perm[itprev - begin];
+            while(itafter != it){
+                moved[itprev - begin] = true;
+                std::iter_swap(itprev, itafter);
+                itprev = itafter;
+                itafter = begin + perm[itafter - begin];
+            }
+            moved[itprev - begin] = true;
+        }
+    }
+};
+
+#endif

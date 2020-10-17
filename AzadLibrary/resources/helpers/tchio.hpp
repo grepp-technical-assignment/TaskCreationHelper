75fd0562bc0b084df6149e2d2d390af167a8aeea
Azad <spongbob9876@naver.com>
2020-10-17 17:15:59 +0900

    Support C/C++ conversion feature

--- AzadLibrary/resources/helpers/tchio.hpp
+++ AzadLibrary/resources/helpers/tchio.hpp
@@ -23,41 +23,69 @@ namespace TCH{
 
     // Secondary template of Data for zero dimensional primitive value.
     template <typename t> class Data<t, 0>{ public:
-        using thistype = t;
+        using thisCppType = t;
+        using thisCType = thisCppType;
 
-        inline static thistype get(std::istream &in){
-            thistype value;
+        inline static thisCppType get(std::istream &in){
+            thisCppType value;
             in >> value;
             return value;
         }
 
-        inline static void put(std::ostream &out, const thistype &data){
+        inline static void put(std::ostream &out, const thisCppType &data){
             out << data << '\n';
         }
+
+        inline static thisCType convert_cpp_c(const thisCppType &data){
+            return data;
+        }
+
+        inline static thisCppType convert_c_cpp(const thisCType &data){
+            return data;
+        }
+
+        inline static void superfree(thisCType ptr){} // Do nothing
     };
 
     // 3rd template of Data for string primitive value.
     template <> class Data<std::string, 0>{ public:
-        using thistype = std::string;
+        using thisCppType = std::string;
+        using thisCType = char*;
 
-        inline static thistype get(std::istream &in){
+        inline static thisCppType get(std::istream &in){
             std::string result;
             size_t size = Data<size_t, 0>::get(in);
             while(size--) result += (char)Data<int, 0>::get(in);
             return result;
         }
 
-        inline static void put(std::ostream &out, const thistype &data){
+        inline static void put(std::ostream &out, const thisCppType &data){
             Data<size_t, 0>::put(out, data.length());
             for(char c: data) Data<int, 0>::put(out, (int)c);
+        }
+
+        inline static thisCType convert_cpp_c(const thisCppType &data){
+            char* ptr = new char[data.length() + 1];
+            for(size_t i=0; i<data.length(); i++) ptr[i] = data[i];
+            ptr[data.length()] = 0;
+            return ptr;
+        }
+
+        inline static thisCppType convert_c_cpp(const thisCType &data){
+            return std::string(data);
+        }
+
+        inline static void superfree(thisCType ptr){
+            delete[] ptr;
         }
     };
 
     // 3rd template of Data for boolean primitive value.
     template <> class Data<bool, 0>{ public:
-        using thistype = bool;
+        using thisCppType = bool;
+        using thisCType = bool;
 
-        inline static thistype get(std::istream &in){
+        inline static thisCppType get(std::istream &in){
             std::string msg; 
             in >> msg;
             if(msg != "true" && msg != "false")
@@ -65,26 +93,64 @@ namespace TCH{
             return msg == "true";
         }
 
-        inline static void put(std::ostream &out, const thistype &data){
+        inline static void put(std::ostream &out, const thisCppType &data){
             out << (data ? "true" : "false") << '\n';
         }
+
+        inline static thisCType convert_cpp_c(const thisCppType &data){
+            return data;
+        }
+
+        inline static thisCppType convert_c_cpp(const thisCType &data){
+            return data;
+        }
+
+        inline static void superfree(thisCType ptr){} // Do nothing
     };
 
     // Primary template of Data for 1+ dimensional array.
     template <typename t, unsigned int dimension> class Data{ public:
-        using prevtype = typename Data<t, dimension-1>::thistype;
-        using thistype = std::vector<prevtype>;
+        using prevCppType = typename Data<t, dimension-1>::thisCppType;
+        using thisCppType = std::vector<prevCppType>;
+        using prevCType = typename Data<t, dimension-1>::thisCType;
+        using thisCType = prevCType*;
 
-        inline static thistype get(std::istream &in){
+        // Parse data from given input stream and return.
+        inline static thisCppType get(std::istream &in){
             size_t size = Data<size_t, 0>::get(in);
-            thistype result;
+            thisCppType result;
             while(size--) result.push_back(Data<t, dimension-1>::get(in));
             return result;
         }
 
-        inline static void put(std::ostream &out, const thistype &data){
+        // Put data to given output stream.
+        inline static void put(std::ostream &out, const thisCppType &data){
             Data<size_t, 0>::put(out, data.size());
-            for(prevtype element: data) Data<t, dimension-1>::put(out, element);
+            for(prevCppType element: data) Data<t, dimension-1>::put(out, element);
+        }
+
+        // Convert C++ data structure into C array.
+        inline static thisCType convert_cpp_c(const thisCppType &data){
+            thisCType ptr = new prevCType*[data.size() + 1];
+            for(size_t i=0; i<data.size(); i++)
+                ptr[i] = Data<t, dimension-1>::convert_cpp_c(data[i]);
+            ptr[data.size()] = NULL;
+            return ptr;
+        }
+
+        // Convert C array into C++ data structure.
+        inline static thisCppType convert_c_cpp(const thisCType &data){
+            thisCppType vec;
+            for(size_t i=0; data[i] != NULL; i++)
+                vec.push_back(Data<t, dimension-1>::convert_c_cpp(data[i]));
+            return vec;
+        }
+
+        // Free C pointer.
+        inline static void superfree(thisCType ptr){
+            for(size_t i=0; ptr[i] != NULL; i++)
+                Data<t, dimension-1>::superfree(ptr[i]);
+            delete[] ptr;
         }
     };
 };

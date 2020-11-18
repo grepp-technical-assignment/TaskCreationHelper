406c0341c4d4911faa70cba7e3e01b63c12883e4
Azad <spongbob9876@naver.com>
2020-11-18 13:15:07 +0900

    Added basePath feature on filesystem

--- AzadLibrary/filesystem.py
+++ AzadLibrary/filesystem.py
@@ -17,7 +17,7 @@ from .misc import randomName, formatPathForLog
 from .errors import TempFileSystemClosed
 
 
-def ThreadsafeDecoratorTFS(method):
+def TFSThreadSafe(method):
     """
     Threadsafe decorator for TempFileSystem.
     """
@@ -39,6 +39,24 @@ def checkIfClosed(method):
                           TempFileSystemClosed)
         else:
             return method(self, *args, **kwargs)
+    return innerFunc
+
+
+def checkBasePath(method):
+    """
+    Check if given path is inside of TempFileSystem's path.
+    If basePath is not given, then `basePath := self.path`.
+    """
+
+    def innerFunc(self, *args, basePath: Path = None, **kwargs):
+        if basePath is not None:
+            if not basePath.relative_to(self.path):
+                raise OSError(
+                    "Given basePath \"%s\" is not inside of TFS path \"%s\"" %
+                    (basePath, self.path))
+        else:
+            basePath = self.path
+        return method(self, *args, basePath=basePath, **kwargs)
     return innerFunc
 
 
@@ -79,15 +97,13 @@ class TempFileSystem:
         Get complete file/directory name with given prefix and extension.
         """
         if namePrefix is not None:
-            prefix = namePrefix
-            if not namePrefix.endswith("/"):
-                prefix += "_"
+            prefix = namePrefix + "_"
         else:
             prefix = ""
         suffix = ("." + extension if extension is not None else "")
         return prefix + name + suffix
 
-    def __contains(self, name: typing.Union[str, Path]):
+    def contains(self, name: typing.Union[str, Path]):
         """
         Return true if there is any file/folder
         with same name in this file system.
@@ -95,13 +111,14 @@ class TempFileSystem:
         if isinstance(name, str):
             return (self.path / name).exists()
         elif isinstance(name, Path):
-            return name.exists() and self.path in name.parents
+            return name.exists() and name.relative_to(self.path)
         else:
             raise TypeError("Invalid name type %s" % (type(name),))
 
-    def __findFeasibleName(
+    @checkBasePath
+    def __findFeasiblePath(
             self, extension: str = None, namePrefix: str = None,
-            randomNameLength: int = None) -> str:
+            randomNameLength: int = None, basePath: Path = None) -> Path:
         """
         Find any feasible path for new file or folder's name.
         """
@@ -110,20 +127,21 @@ class TempFileSystem:
             if randomNameLength is None else randomNameLength
 
         for _ in range(self.DefaultRandomTryIterationLimit):
-            tempPath = self.getName(
+            tempPath = basePath / self.getName(
                 randomName(length), extension=extension,
                 namePrefix=namePrefix)
-            if not self.__contains(tempPath):
+            if not self.contains(tempPath):
                 return tempPath
         raise OSError("Couldn't find feasible path in %d iterations" %
                       (self.DefaultRandomTryIterationLimit,))
 
     @checkIfClosed
-    @ThreadsafeDecoratorTFS
+    @checkBasePath
+    @TFSThreadSafe
     def newTempFile(
             self, content: typing.Union[str, bytes] = None,
             name: str = None, extension: str = None,
-            namePrefix: str = None) -> Path:
+            namePrefix: str = None, basePath: Path = None) -> Path:
         """
         Make new file under this directory.
         """
@@ -131,49 +149,48 @@ class TempFileSystem:
 
         # Determine name
         if name is not None:
-            name = self.getName(name, extension=extension,
-                                namePrefix=namePrefix)
-            if self.__contains(name):
+            path = basePath / self.getName(
+                name, extension=extension, namePrefix=namePrefix)
+            if self.contains(path):
                 raise FileExistsError
         else:
-            name = self.__findFeasibleName(
-                extension=extension, namePrefix=namePrefix)
+            path = self.__findFeasiblePath(
+                extension=extension, namePrefix=namePrefix,
+                basePath=basePath)
 
         # Write content and return
-        filepath = self.path / name
-        with open(filepath, wmode) as file:
+        with open(path, wmode) as file:
             if content is not None:
                 file.write(content)
-        return filepath
+        return path
 
     @checkIfClosed
-    @ThreadsafeDecoratorTFS
+    @checkBasePath
+    @TFSThreadSafe
     def newFolder(self, name: str = None, namePrefix: str = None,
-                  depth: int = 1) -> Path:
+                  basePath: Path = None) -> Path:
         """
         Make new folder under this directory.
         If name is not None, then the depth will be ignored.
         """
-        # Determine name
+        # Determine path
         if name is not None:
-            name = self.getName(name, namePrefix=namePrefix)
-            if self.__contains(name):
+            path = basePath / self.getName(name, namePrefix=namePrefix)
+            if self.contains(path):
                 raise OSError("Directory already exists")
         else:
-            if not isinstance(depth, int) or depth < 1:
-                raise ValueError("Invalid value depth %s" % (depth,))
-            name = self.__findFeasibleName(namePrefix=namePrefix)
-            (self.path / name).mkdir()
-            for _ in range(1, depth):
-                name = self.__findFeasibleName(namePrefix=name + "/")
-                (self.path / name).mkdir()
+            path = self.__findFeasiblePath(
+                namePrefix=namePrefix, basePath=basePath)
 
-        return self.path / name
+        path.mkdir()
+        return path
 
     @checkIfClosed
-    @ThreadsafeDecoratorTFS
-    def copyFile(self, source: typing.Union[str, Path], destName: str = None,
-                 extension: str = None, namePrefix: str = None):
+    @checkBasePath
+    @TFSThreadSafe
+    def copyFile(self, source: typing.Union[str, Path],
+                 destName: str = None, extension: str = None,
+                 namePrefix: str = None, basePath: Path = None) -> Path:
         """
         Copy external file into current TempFileSystem.
         """
@@ -184,35 +201,36 @@ class TempFileSystem:
 
         # Get name
         if destName is not None:
-            name = self.getName(destName, extension=extension,
-                                namePrefix=namePrefix)
-            if self.__contains(name):
+            path = basePath / self.getName(
+                destName, extension=extension, namePrefix=namePrefix)
+            if self.contains(path):
                 raise FileExistsError
         else:
-            name = self.__findFeasibleName(
-                extension=extension, namePrefix=namePrefix)
+            path = self.__findFeasiblePath(
+                extension=extension, namePrefix=namePrefix,
+                basePath=basePath)
 
         # Fast copy using shutil
-        shutil.copyfile(source, self.path / name)
-        return self.path / name
+        shutil.copyfile(source, path)
+        return path
 
     @checkIfClosed
-    @ThreadsafeDecoratorTFS
-    def pop(self, name: typing.Union[str, Path], b: bool = True) -> \
+    @TFSThreadSafe
+    def pop(self, path: typing.Union[str, Path], b: bool = True) -> \
             typing.Union[str, bytes, None]:
         """
         Delete file or folder by given name.
         If given name is file, return file content.
         """
-        if isinstance(name, str):
-            path: Path = self.path / name
-        elif isinstance(name, Path):
-            path = name
+        if isinstance(path, str):
+            path: Path = self.path / path
+        elif isinstance(path, Path):
+            pass
         else:
-            raise TypeError("Invalid name type %s" % (type(name),))
+            raise TypeError("Invalid name type %s" % (type(path),))
 
-        if not self.__contains(name):
-            raise FileNotFoundError("name = %s" % (name,))
+        if not self.contains(path):
+            raise FileNotFoundError("name = %s" % (path,))
         elif path.is_file():  # File
             with open(path, "rb" if b else "r") as file:
                 content = file.read()
@@ -223,7 +241,7 @@ class TempFileSystem:
             return None
 
     @checkIfClosed
-    @ThreadsafeDecoratorTFS
+    @TFSThreadSafe
     def close(self):
         """
         Close file system by deleting everything.
